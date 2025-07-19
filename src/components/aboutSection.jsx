95dd6798af456f8aeb6f3845d9116c64f5fb4a7d
rzalwhy12 <rzalcorp05@gmail.com>
2025-07-20 00:28:04 +0700

    perbaikan testi dan about

--- src/components/aboutSection.jsx
+++ src/components/aboutSection.jsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 
 const AboutSection = () => {
     return (
-        <section className="flex flex-col lg:flex-row items-center justify-between px-6 py-16 lg:px-20 bg-white">
+        <section className="flex flex-col lg:flex-row items-center justify-between py-30 bg-white">
             {/* Left Image */}
             <div className="w-full lg:w-1/2 mb-10 lg:mb-0">
-                <div className="rounded-3xl overflow-hidden">
+                <div className="rounded-tr-3xl overflow-hidden">
                     <Image src="/image/about.png" // Pastikan file gambar kamu dinamai seperti ini dan ada di folder
                         alt="Team discussing" width={800} height={600} className="object-cover w-full h-full" />
                 </div>
             </div>
 
             {/* Right Text */}
-            <div className="w-full lg:w-1/2 lg:pl-16">
+            <div className="w-full lg:w-1/2 lg:pr-16">
                 <div className="mb-4 h-1 w-30 bg-gradient-to-r from-orange-500 to-black rounded-full" />
                 <h2 className="text-3xl font-bold mb-4 text-black">About GGC</h2>
                 <p className="text-gray-600 mb-8">
