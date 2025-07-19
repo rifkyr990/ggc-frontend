1574e04ebf80d726b669836eac6141c5a56e20b9
rzalwhy12 <rzalcorp05@gmail.com>
2025-07-20 00:56:10 +0700

    perbaikan about

--- src/components/aboutSection.jsx
+++ src/components/aboutSection.jsx
@@ -5,15 +5,18 @@ const AboutSection = () => {
     return (
         <section className="flex flex-col lg:flex-row items-center justify-between py-30 bg-white">
             {/* Left Image */}
-            <div className="w-full lg:w-1/2 mb-10 lg:mb-0">
-                <div className="rounded-tr-3xl overflow-hidden">
-                    <Image src="/image/about.png" // Pastikan file gambar kamu dinamai seperti ini dan ada di folder
-                        alt="Team discussing" width={800} height={600} className="object-cover w-full h-full" />
+            <div className="w-full md:w-1/2 h-80 md:h-[600px] flex-shrink-0 flex items-center justify-center relative">
+                <div className="relative w-full h-80 md:h-[600px]">
+                    <img
+                        src="/image/about.png"
+                        alt="About GGC"
+                        className="w-full h-80 md:h-[600px] object-cover rounded-tr-4xl relative z-1"
+                    />
                 </div>
             </div>
 
             {/* Right Text */}
-            <div className="w-full lg:w-1/2 lg:pr-16">
+            <div className="w-full lg:w-1/2 lg:pr-16 ml-10">
                 <div className="mb-4 h-1 w-30 bg-gradient-to-r from-orange-500 to-black rounded-full" />
                 <h2 className="text-3xl font-bold mb-4 text-black">About GGC</h2>
                 <p className="text-gray-600 mb-8">
